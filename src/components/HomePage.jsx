6c9ff64b79f7d68272d0f35433ae659f2e96a99d
De-faked <espam475@gmail.com>
2024-07-23 14:56:19 +0200

    games components removed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './HomePage.css';
-import Counter from './Counter';
-import RockPaperScissors from './RockPaperScissors';
-import TicTacToe from './TicTacToe';
 import PlantPage from './PlantPage';
 
 const HomePage = () => {
@@ -13,22 +10,10 @@ const HomePage = () => {
         <p>Explore our interactive games and learn about plants!</p>
       </header>
       <main className="game-sections">
-        {/* <div className="game-card">
-          <h2>Counter Game</h2>
-          <Counter />
-        </div>
-        <div className="game-card">
-          <h2>Rock Paper Scissors</h2>
-          <RockPaperScissors />
-        </div>
-        <div className="game-card">
-          <h2>Tic Tac Toe</h2>
-          <TicTacToe />
-        </div>
         <div className="game-card">
           <h2>Random Plant</h2>
           <PlantPage />
-        </div> */}
+        </div>
       </main>
       <footer className="home-footer">
         <p>&copy; 2024 Games Portal. All rights reserved.</p>
